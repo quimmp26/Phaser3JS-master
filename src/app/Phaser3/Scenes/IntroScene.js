31cdce6e982d2c0549769ec8e00c9986d746bb48
quimmp26 <quiim2000@gmail.com>
2021-03-23 16:21:29 +0100

    ok

--- src/app/Phaser3/Scenes/IntroScene.js
+++ src/app/Phaser3/Scenes/IntroScene.js
@@ -21,7 +21,7 @@ class IntroScene extends Phaser.Scene {
 
     //this.info = this.add.image(400, 300, 'info2').setOrigin(0.5, 0.5).setScale(0.5);
 
-    this.text = this.add.text(17, 240, 'Hoy es el primer día de tu trabajo en la construcción después del último accidente que tuviste hace 6 meses. ¡Aún puedes sentir el profundo dolor que te causó en el pie aquel maldito clavo! Sin embargo, empiezas con muchas ganas.¡Intenta ganar el máximo número de monedas! Pero recuerda, debes evitar los obstáculos, como la caída de ladrillos o los clavos del suelo y para finalizar tu partida deberás llegar al final del mapa i recoger los equipos de protección individual, el CASCO, el CHALECO i las BOTAS. A por ello!', {
+    this.text = this.add.text(17, 200, 'Hoy es el primer día de tu trabajo en la construcción después del último accidente que tuviste hace 6 meses. ¡Aún puedes sentir el profundo dolor que te causó en el pie aquel maldito clavo! Sin embargo, empiezas con muchas ganas.¡Intenta ganar el máximo número de monedas! Pero recuerda, debes evitar los obstáculos, como la caída de ladrillos o los clavos del suelo y para finalizar tu partida deberás llegar al final del mapa i recoger los equipos de protección individual, el CASCO, el CHALECO i las BOTAS. A por ello!', {
       fontSize: "20px",
       fill: "#ffff00",
       fontFamily: 'Font1',
